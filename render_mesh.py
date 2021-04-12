480d17148f306fa38715d62ac861c107a9fee3f9
Katie <katiemc@mit.edu>
2021-04-11 21:25:59 -0400

    updated unconditional generation sample testing and found new good render views

--- render_mesh.py
+++ render_mesh.py
@@ -14,5 +14,8 @@ mesh_path = "/om/user/katiemc/occupancy_networks/out/unconditional/onet_multi_si
 # some bottom: python3 obj2png/src/obj2png.py -i sample.obj -o sample.png -a 270 -e 40
 # some aspect of front + depth
 
+# could use python3 obj2png/src/obj2png.py -i sample2.obj -o sample2.png -a 0 -e 0
+#   and rotate -45 degrees
+
 
 python model-converter-python/convert.py -i=out/unconditional/chairs1/generation/meshes/03001627/1007e20d5e811b308351982a6e40cf41.off -o=/om/user/katiemc/occupancy_networks/sample.obj
--- streamlined_model_eval.py
+++ streamlined_model_eval.py
@@ -2,20 +2,31 @@
 Custom script for streamlined I/O with model
 '''
 
+# TODO: vary number of samples we draw
+
 import torch
 from im2mesh.checkpoints import CheckpointIO
 from im2mesh.config import load_config
 from im2mesh import config, data
 
 cfg_dir = "/om/user/katiemc/occupancy_networks/configs/unconditional/sample_complexity/"
-cfg_file = cfg_dir + "chair_subset500_orig.yaml"
+# cfg_file = cfg_dir + "single_chair.yaml"
+cfg_file = cfg_dir + "chair_subset4000.yaml"
+
 cfg = load_config(cfg_file, 'configs/default.yaml')
 
 is_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if is_cuda else "cpu")
 
 test_dataset = config.get_dataset('test', cfg)
+test_loader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=1, num_workers=0, shuffle=False)
 model = config.get_model(cfg, device=device, dataset=test_dataset)
+
+out_dir = cfg['training']['out_dir']
+checkpoint_io = CheckpointIO(out_dir, model=model)
+checkpoint_io.load(cfg['test']['model_file'])
+
 
 z = model.get_z_from_prior() # shape = torch.Size([128])
 
@@ -29,11 +40,18 @@ generator = config.get_generator(model, cfg,device)
 # TODO: check mesh from latent (and how to go from mesh => png!)
 # mesh = generator.generate_from_latent(z)
 
+data = list(test_loader)[0]
+mesh,_ = generator.generate_mesh(data)
+mesh.export("sample_mesh.off")
+
 model.eval()
 stats_dict = {}
 inputs = torch.empty(1, 0).to(device)
 kwargs = {}
-with torch.no_grad():
-    c = model.encode_inputs(inputs)
+with torch.no_grad(): c = model.encode_inputs(inputs)
+
 z = model.get_z_from_prior((1,), sample=2048).to(device)
 mesh = generator.generate_from_latent(z, c)
+mesh.export("sample_mesh.off")
+
+
